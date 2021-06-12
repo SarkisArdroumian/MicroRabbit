68152360b71e233a8600eced64a140bde946b512
Sarkis Ardroumian <sarkis.ardroumian@orbility.com>
2021-06-12 17:59:21 +0300

    Implemented publish and send command for banking microservice

--- MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,7 +1,10 @@
-﻿using Microrabbit.Banking.Data.Context;
+﻿using MediatR;
+using Microrabbit.Banking.Data.Context;
 using Microrabbit.Banking.Data.Repository;
 using MicroRabbit.Banking.Application.Interfaces;
 using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Domain.CommandHandlers;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
@@ -18,6 +21,9 @@ namespace MicroRabbit.Infra.IoC
         {
             //Domain Bus
             services.AddTransient<IEventBus, RabbitMQBus>();
+
+            //Domain Banking Commands
+            services.AddTransient<IRequestHandler<CreateTransferCommand, bool>, TranfserCommandHandler>();
 
             //Application  Services
             services.AddTransient<IAccountService, AccountService>();
